c6301daa83079e9e9b1d4acc1425766cc96947dc
Parocq <55479321+Parocq@users.noreply.github.com>
2021-04-07 12:50:09 +0300

    Initial commit

---
+++ resmodule/src/main/java/com/bsuir/herman/resmodule/Const.java
@@ -0,0 +1,7 @@
+package com.bsuir.herman.resmodule;
+
+public class Const {
+
+    public static String text = "res_module_text";
+
+}
---
+++ resmodule/src/main/java/com/bsuir/herman/resmodule/TestResActivity.kt
@@ -0,0 +1,13 @@
+package com.bsuir.herman.resmodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestResActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_res)
+
+
+    }
+}
